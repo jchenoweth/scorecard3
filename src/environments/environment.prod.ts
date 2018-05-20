f280083fd629259d46c297a6e1ccf29db6fc42a5
J Chenoweth <jchenoweth1@gmail.com>
2018-05-20 16:40:14 -0500

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyCUDwobcND0h-0_qHb6bjzfCAteHJpKuRo',
+  authDomain: 'golfscorecard-d9bac.firebaseapp.com',
+  databaseURL: 'https://golfscorecard-d9bac.firebaseio.com',
+  storageBucket: 'golfscorecard-d9bac.appspot.com',
+  messagingSenderId: '758588685657'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,6 @@ export const environment = {
   apiKey: 'AIzaSyCUDwobcND0h-0_qHb6bjzfCAteHJpKuRo',
   authDomain: 'golfscorecard-d9bac.firebaseapp.com',
   databaseURL: 'https://golfscorecard-d9bac.firebaseio.com',
-  projectId: 'golfscorecard-d9bac',
   storageBucket: 'golfscorecard-d9bac.appspot.com',
   messagingSenderId: '758588685657'
 };
