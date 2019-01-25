f6f7bd9c06643cc63c8c782ff57bb4bc553f1038
jchenoweth <jchenoweth1@gmail.com>
2019-01-24 19:44:15 -0600

    updates to course maint component

--- src/app/course-maint/course-maint.component.ts
+++ src/app/course-maint/course-maint.component.ts
@@ -1,3 +1,4 @@
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
 
 import { CourseService } from './../shared/course.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,6 +10,7 @@ import { Course } from '../shared/course.model';
   styleUrls: ['./course-maint.component.css']
 })
 export class CourseMaintComponent implements OnInit {
+  courseForm: FormGroup;
   courseName = 'This Course';
   nbrOfHoles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   holePar: number[];
@@ -16,7 +18,31 @@ export class CourseMaintComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.courseForm = new FormGroup ({
+      'courseName': new FormControl(null),
+      'parForHole1': new FormControl(),
+      'parForHole2': new FormControl(),
+      'parForHole3': new FormControl(),
+      'parForHole4': new FormControl(),
+      'parForHole5': new FormControl(),
+      'parForHole6': new FormControl(),
+      'parForHole7': new FormControl(),
+      'parForHole8': new FormControl(),
+      'parForHole9': new FormControl(),
+      'parForHole10': new FormControl(),
+      'parForHole11': new FormControl(),
+      'parForHole12': new FormControl(),
+      'parForHole13': new FormControl(),
+      'parForHole14': new FormControl(),
+      'parForHole15': new FormControl(),
+      'parForHole16': new FormControl(),
+      'parForHole17': new FormControl(),
+      'parForHole18': new FormControl()
+    });
+  }
 
+  onSubmit() {
+    console.log(this.courseForm);
   }
 
   onParClicked() {
